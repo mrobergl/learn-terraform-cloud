7970c4b293521eb83a8f85ba66ccda72d4cc486c
mrobergl <mrobergl@hotmail.com>
2022-05-03 05:39:10 +0000

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "hashicorp-learn"
+  # cloud {
+  #   organization = "elaheh-org"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
